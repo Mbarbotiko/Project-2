b63d5f902db492110c38c30f12a6ca4d17d8a845
Al Bassett <albassett@Als-MacBook-Pro.local>
2018-06-26 15:30:05 -0500

    rough idea of post form

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,13 +1,13 @@
 var path = require("path");
+
 module.exports = function(app){
+    
     app.get("/", function (req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.get("/post", function(req, res){
-        res.sendFile(path.join(__dirname, 
-            "../public/post.html"
-        ));
+        res.sendFile(path.join(__dirname, "../public/post.html"));
     });
 
     app.get("/view", function(req, res){
