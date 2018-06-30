a045db1766f43450c3f3795dadcfa89ebd4f30ee
Al Bassett <albassett@er-10-129-202-164.wireless.umn.edu>
2018-06-30 13:53:21 -0500

    updating local

--- public/js/post-items.js
+++ public/js/post-items.js
@@ -31,7 +31,38 @@ $( document ).ready(function() {
             return console.log("Don't stop til you get enough.");
         }
         console.log(newItem);
-    })
+    });
+    var userSelect = $("#user");
+    
+      // A function to get Authors and then render our list of Authors
+    function getAuthors() {
+        $.get("/api/users", renderAuthorList);
+    }
+    // Function to either render a list of authors, or if there are none, direct the user to the page
+    // to create an author first
+    function renderAuthorList(data) {
+        if (!data.length) {
+        window.location.href = "/NewUser.html";
+    }
+    $(".hidden").removeClass("hidden");
+    var rowsToAdd = [];
+    for (var i = 0; i < data.length; i++) {
+      rowsToAdd.push(createAuthorRow(data[i]));
+    }
+    authorSelect.empty();
+    console.log(rowsToAdd);
+    console.log(authorSelect);
+    authorSelect.append(rowsToAdd);
+    authorSelect.val(authorId);
+  }
+
+    // Creates the author options in the dropdown
+    function createAuthorRow(author) {
+        var listOption = $("<option>");
+        listOption.attr("value", user.id);
+        listOption.text(user.name);
+        return listOption;
+      }
 
 });
 
