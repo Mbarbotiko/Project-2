04088194976c392def64ab213c6959e62a6e43ed
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-07-01 16:28:02 -0500

    adding commented out code for another day for sequelize function

--- public/js/view-all.js
+++ public/js/view-all.js
@@ -29,32 +29,58 @@ $(document).ready(function () {
                     `</div>`);
             });
 
+            var selectedItems = []
+            function emptyselectedItemsArr() {
+                selectedItems = [];
+            }
+        
+            $(document.body).on('click', '.material-icons', function () {
+                selectedItems.push($(this).attr('id'));
+                // $($(this)).css({"background-color":"pink"})
+                var itemOne = selectedItems[0];
+                var itemTwo = selectedItems[1];
+                console.log("First item chosen ID: "+itemOne);
+                console.log("Second item chosen ID: "+itemTwo);
+                // console.log(res.id+itemOne);
+                // console.log(res.id+itemTwo);
+                if (selectedItems.length == 2) {
+                    confirm("Press a button! OK SWAP CANCEL GO BACK AND PICK");
+                    if (confirm == true) {
+                        //code here to handle swap in SQL
+        
+                        // To update several rows at once:
+        
+                        // Task.bulkCreate([
+                        //     { subject: 'programming', status: 'executing' },
+                        //     { subject: 'reading', status: 'executing' },
+                        //     { subject: 'programming', status: 'finished' }
+                        // ]).then(() => {
+                        //     return Task.update(
+                        //         { status: 'inactive' }, /* set attributes' value */
+                        //         { where: { subject: 'programming' } } /* where criteria */
+                        //     );
+                        // }).spread((affectedCount, affectedRows) => {
+                        //     // .update returns two values in an array, therefore we use .spread
+                        //     // Notice that affectedRows will only be defined in dialects which support returning: true
+        
+                        //     // affectedCount will be 2
+                        //     return Task.findAll();
+                        // }).then(tasks => {
+                        //     console.log(tasks) // the 'programming' tasks will both have a status of 'inactive'
+                        // })
+        
+        
+                    } else {
+                        emptyselectedItemsArr();
+                        console.log(selectedItems);
+        
+                    }
+                }
+        
+        
+            });
+
 
         })
-
-
-    var selectedItems = []
-    function emptyselectedItemsArr() {
-        selectedItems = [];
-    }
-
-    $(document.body).on('click', '.material-icons', function () {
-        selectedItems.push($(this).attr('id'));
-        // $($(this)).css({"background-color":"pink"})
-        console.log(" First Item ID is: " + selectedItems[0] + " Second Item ID is: " + selectedItems[1]);
-        if (selectedItems.length == 2) {
-            confirm("Press a button! OK SWAP CANCEL GO BACK AND PICK");
-            if (confirm == true) {
-                //write code here to swap items with these ID's in the database
-
-            } else {
-                emptyselectedItemsArr();
-
-            }
-        }
-
-
-    });
-
 
 });
