2fd24cd483c121ea5d3a73732029af3e54e1c5cf
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-07-03 16:28:26 -0500

    adding modal open on item select, adding modal cancel and close, and ajax call for selected item showing

--- public/js/view-all.js
+++ public/js/view-all.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+
     var cardContainer = $(".card-items")
+    var showUsersSelection = $(".card-smallSelection")
     var queryURL = "http://localhost:8080/api/items";
     $.ajax({
         url: queryURL,
@@ -28,42 +30,69 @@ $(document).ready(function () {
                     `</div>`);
             });
 
-                var selectedItems = []
-                function emptyselectedItemsArr() {
-                    selectedItems = [];
-                }
+            var selectedItems = []
+            function emptyselectedItemsArr() {
+                selectedItems = [];
+            }
 
-                $(document.body).on('click', '.material-icons', function () {
-                    selectedItems.push($(this).attr('id'));
-                    // $($(this)).css({"background-color":"pink"})//just messing around with showing user that the item is selected, worry about this later after swap function is working.
-                    var itemOne = selectedItems[0];
-                    var itemTwo = selectedItems[1];
-                    console.log("First item chosen ID: " + itemOne);
-                    console.log("Second item chosen ID: " + itemTwo);
+            $(document.body).on('click', '.material-icons', function () {
+                selectedItems.push($(this).attr('id'));
+                // $($(this)).css({"background-color":"pink"})//just messing around with showing user that the item is selected, worry about this later after swap function is working.
+                var itemOne = selectedItems[0];
+                var itemTwo = selectedItems[1];
+                console.log("First item chosen ID: " + itemOne);
+                console.log("Second item chosen ID: " + itemTwo);
 
-                    if (selectedItems.length == 2) {
-                        var wasConfirmed = confirm("Press a button! OK SWAP CANCEL GO BACK AND PICK");
-                        if (wasConfirmed) {
-                            $.ajax({
-                                url: "http://localhost:8080/api/swap",
-                                method: 'POST',
-                                data: {
-                                    itemOne:itemOne,
-                                    itemTwo:itemTwo
-                                }
-                                //put an auto refreshin here
-                            }).then(console.log);
-                        } else {
-                            emptyselectedItemsArr();
-                            console.log(selectedItems);
-                            //also close modal here
+                if (selectedItems.length == 1) {
+                    $('.modal').modal("open");
+                    $.ajax({
+                        url: "http://localhost:8080/api/items/" + itemOne,
+                        method: 'GET'
+                    })
+                        .then(function (res) {
+                            showUsersSelection.html(`<div class="col s12 m6 l4">` +
+                                `<div class="card">` +
+                                `<div class="card-image">` +
+                                `<img src='${res.picture}' alt='Item Picture'>` + `<span class="card-title">${res.item}</span>` +
+                                `</div>` +
+                                `<div class="card-content ">` +
+                                `<p>${res.description}</p>` + `<p>Category: ${res.category}<p>` + `<p>Posted by: ${res.User.name}</p>` +
+                                `</div>` +
+                                `</div>` +
+                                `</div>`)
 
-                        }
+                        });
+
+                    $(document.body).on('click', '#cancel-button', function (){
+                        $('.modal').modal("close");
+                        emptyselectedItemsArr();
+                        
                     }
 
+                    )
 
-                });
-        
+
+                    // if (wasConfirmed) {
+                    //     $.ajax({
+                    //         url: "http://localhost:8080/api/swap",
+                    //         method: 'POST',
+                    //         data: {
+                    //             itemOne:itemOne,
+                    //             itemTwo:itemTwo
+                    //         }
+                    //         //put an auto refreshin here
+                    //     }).then(console.log);
+                    // } else {
+                    //     emptyselectedItemsArr();
+                    //     console.log(selectedItems);
+                    //     //also close modal here
+
+                    // }
+                }
+
+
+            });
+
 
 
 
