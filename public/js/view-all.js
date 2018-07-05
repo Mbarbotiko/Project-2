b32adfb768b7b8041716a21b99b46c52f26678a9
unknown <stephen.faunillan@gmail.com>
2018-07-05 18:28:09 -0500

    fixed cards and html issues

--- public/js/view-all.js
+++ public/js/view-all.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('#confirm-button').hide();
-    var cardContainer = $(".card-items");
-    var showUsersSelection = $(".card-smallSelection");
-    var showMyStuff = $('.card-smallShowMyStuff');
+    var cardContainer = $(".card-items")
+    var showUsersSelection = $(".card-smallSelection")
+    var showMyStuff = $('.card-smallShowMyStuff')
     var queryURL = "http://localhost:8080/api/items";
     $.ajax({
         url: queryURL,
@@ -17,6 +17,7 @@ $(document).ready(function () {
                 var allCategories = printEverything.category;
                 var allImages = pictureIMG;
                 var allUsersNames = printEverything.User.name;
+                var icon = `<a class="btn-floating halfway-fab waves-effect waves-light green"><i class="material-icons" id=${printEverything.id}>swap_calls</i></a>`
                 cardContainer.append(
                     `<div class="col s12 m6 l4">` +
                     `<div class="card">` +
@@ -29,18 +30,15 @@ $(document).ready(function () {
                     `</div>` +
                     `</div>`);
             });
-
             var selectedItems = []
             function emptyselectedItemsArr() {
                 selectedItems = [];
             }//create array to store user clicked items, also a function to clear the array
-
             $(document.body).on('click', '.material-icons', function () {
                 //on click of the material icons collect the attributes which are set to the ID of the item set by database.
                 selectedItems.push($(this).attr('id'));
                 var itemOne = selectedItems[0];
                 var itemTwo = selectedItems[1];
-
                 if (selectedItems.length == 1) {
                     //open the modal when user selects one item they want
                     $('.modal').modal("open");
@@ -59,10 +57,7 @@ $(document).ready(function () {
                                 `</div>` +
                                 `</div>` +
                                 `</div>`)
-
                         });
-
-
                     var queryURL = "http://localhost:8080/api/users";
                     $.ajax({
                         url: queryURL,
@@ -70,39 +65,26 @@ $(document).ready(function () {
                     }).then(function (data) {
                         renderUserList(data);
                     });
-
                     // A function to get users and then render our list of users
                     var userSelect = $("#user");
-
                     function renderUserList(data) {
-
                         var rowsToAdd = [];
-
                         var selectBox = $("#user");
-
                         for (var i = 0; i < data.length; i++) {
                             selectBox.append(createUserRow(data[i]));
                             // oncancel this needs to be cleared its appending over and over again to the list.
-
                             //   https://stackoverflow.com/questions/47824/how-do-you-remove-all-the-options-of-a-select-box-and-then-add-one-option-and-se
-
                         }
                     };
-
                     // Creates the user options in the dropdown
                     function createUserRow(user) {
-
                         var listOption = $("<option>");
-
                         listOption.attr("value", user.id);
                         listOption.text(user.name);
                         return listOption;
-
                     };
-
                     $('#user').on('change', function () {
                         var userSelectedItem = $(this).val();
-
                         $.ajax({
                             url: "http://localhost:8080/api/users/" + userSelectedItem,
                             method: 'GET'
@@ -110,35 +92,25 @@ $(document).ready(function () {
                             .then(function (res) {
                                 res.Items.forEach(function (printUsersItems) {
                                     var icon = `<a class="btn-floating halfway-fab waves-effect waves-light green"><i class="material-icons" id=${printUsersItems.id}>swap_calls</i></a>`
-
                                     showUsersSelection.append(`<div class="col s12 m6 l4">` +
                                         `<div class="card">` +
                                         `<div class="card-image">` +
                                         `<img src='${printUsersItems.picture}' alt='Item Picture'>` + `<span class="card-title">${printUsersItems.item}</span>` + icon +
                                         `</div>`);
-
                                 });
                             });
-
-
                     });
-
                 }
-
                 $(document.body).on('click', '#cancel-button', function () {
                     $('.modal').modal("close");
                     $('#confirm-button').hide();
                     emptyselectedItemsArr();
                     showUsersSelection.empty();
                     userSelect.empty();//clicking cancel on the modal clears the user selection array and closes the modal.
-
                 })
-
-                if(selectedItems.length===2){
+                if (selectedItems.length === 2) {
                     $('#confirm-button').show();
                 }
-
-
                 $(document.body).on('click', '#confirm-button', function () {
                     $.ajax({
                         url: "http://localhost:8080/api/swap",
@@ -147,15 +119,11 @@ $(document).ready(function () {
                             itemOne: itemOne,
                             itemTwo: itemTwo
                         }
-                        
-                    }).then(console.log);
 
+                    }).then(console.log);
                     //add a loading screen here for 5 seconds then do a reload of the page below:
                     location.reload()
                 })
             });
-
-
         })
-
 });
